b4f671cb5dd23449dc83fb2613e40cec9b9348fd
김현희 <tta_bi@naver.com>
2022-01-05 10:57:09 +0900

    Glide 라이브러리 설치 / 코틀린 코드 작성 - 이미지가 아직 안 불러와짐

--- app/src/main/java/com/nepplus/librarypractice_20220105/MainActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice_20220105/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nepplus.librarypractice_20220105
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,11 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
 
         }
+
+
+//        Glide 이용 => 웹의 이미지를 -> imgRecent에 반영하기
+
+        Glide.with(this).load("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.besthdwallpaper.com%2Feumag%2Fitzy-ryujin-loco-myujigbidio-cwalyeong-hyeonjang-the-album-crazy-in-love-dt_ko-80107.html&psig=AOvVaw0ADN8qIhZ0JFwj_jhjsqbn&ust=1641433781360000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCLCAu7-_mfUCFQAAAAAdAAAAABAD").into(imgRecent)
 
     }
 }
