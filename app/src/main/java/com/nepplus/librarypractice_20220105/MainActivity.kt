5895d9fc3f0cc611606642320769fd3cd3e4239d
김현희 <tta_bi@naver.com>
2022-01-05 15:51:22 +0900

    실제 권한 확인 기능 실행

--- app/src/main/java/com/nepplus/librarypractice_20220105/MainActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice_20220105/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.nepplus.librarypractice_20220105
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,14 +23,33 @@ class MainActivity : AppCompatActivity() {
 
 //            코드 구조가 틀린 건 아님 => 앱이 죽는 상황
 
-//            CALL (바로 전화 연결)에 대한 권한 획득 후에 전화 연결 진행행
+//            CALL (바로 전화 연결)에 대한 권한 획득 후에 전화 연결 진행
+//            pl변수에, 권한 OK일때/ 거부일때 어떤 행동을 할지, 방침만 저장해둠.
+//            실제로 권한을 묻는 행위는 코딩 X
+        val pl = object : PermissionListener{
+            override fun onPermissionGranted() {
 
-           val myUri = Uri.parse("tel:010-0000-0000")
-            val myIntent = Intent( Intent.ACTION_CALL, myUri )
-            startActivity(myIntent)
+//                권한이 OK 되었을 때 해줄 일 적는 공간
+                val myUri = Uri.parse("tel:010-0000-0000")
+                val myIntent = Intent( Intent.ACTION_CALL, myUri )
+                startActivity(myIntent)
+
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(this@MainActivity, "권란이 거부되어 통화가 불가능합니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+//            실제 권한 확인
+            TedPermission.create()
+                .setPermissions(pl)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
 
 
         }
+
+
 
         imgProfilePhoto.setOnClickListener {
 
