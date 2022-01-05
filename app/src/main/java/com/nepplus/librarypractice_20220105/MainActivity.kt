4549a03a01e298843d413290c65a576d5bcf9d00
김현희 <tta_bi@naver.com>
2022-01-05 11:35:18 +0900

    인터넷 사용 권한 추가 - Glide 이미지 로딩 가능

--- app/src/main/java/com/nepplus/librarypractice_20220105/MainActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice_20220105/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
 //        Glide 이용 => 웹의 이미지를 -> imgRecent에 반영하기
 
-        Glide.with(this).load("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.besthdwallpaper.com%2Feumag%2Fitzy-ryujin-loco-myujigbidio-cwalyeong-hyeonjang-the-album-crazy-in-love-dt_ko-80107.html&psig=AOvVaw0ADN8qIhZ0JFwj_jhjsqbn&ust=1641433781360000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCLCAu7-_mfUCFQAAAAAdAAAAABAD").into(imgRecent)
+        Glide.with(this).load("http://url.kr/doc6fx").into(imgRecent)
 
     }
 }
